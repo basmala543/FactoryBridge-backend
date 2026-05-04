ace1b08b68644b9d91c257d4531f942e179163f8
basmala <basmalatarek21@gmail.com>
2026-05-05 02:11:11 +0300

    fix: update cors and add otp

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));app.use(express.json());
 
 // routes
 const authRoutes = require("./routes/auth");
