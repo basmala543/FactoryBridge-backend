4e32524981ff212db2e10246a04419897ff4f49f
basmala <basmalatarek21@gmail.com>
2026-05-05 03:29:24 +0300

    fix: update gmail credentials

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'factorybridge6@gmail.com',
-    pass: 'alec colf cvav owum'
+    user: 'factorybridge7@gmail.com',
+    pass: 'lnfa sluf qogz wjbw'
   }
 });
 
