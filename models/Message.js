a95867db3312b2ba3b0d3b73480e497dbec89ffa
basmala <basmalatarek21@gmail.com>
2026-05-17 01:46:00 +0300

    ..

--- models/Message.js
+++ models/Message.js
@@ -4,7 +4,9 @@ const MessageSchema = new mongoose.Schema({
     senderId: { type: String, required: true },
     receiverId: { type: String, default: 'admin' }, // افتراضاً إن الطرف التاني هو الدعم
     message: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
+    timestamp: { type: Date, default: Date.now },
+    isRead: { type: Boolean, default: false } // ← أضيفي السطر ده بس
+
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
