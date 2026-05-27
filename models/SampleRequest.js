d72a1ff8cbd4500303d2d6ca46fffa5f6c0cd6c2
basmala <basmalatarek21@gmail.com>
2026-05-27 03:30:44 +0300

    ya allah

--- models/SampleRequest.js
+++ models/SampleRequest.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const sampleRequestSchema = new mongoose.Schema({
  brand: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  factory: { type: String, required: true },
-  productName: { type: String, required: true },
+factory: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+ productName: { type: String, required: true },
   quantity: { type: Number, required: true },
   notes: { type: String },
   status: { 
