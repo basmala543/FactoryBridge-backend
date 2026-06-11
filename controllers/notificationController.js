5786fa55ef512888b0dea88601dd114c705af405
basmala <basmalatarek21@gmail.com>
2026-06-12 02:06:35 +0300

    again

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -9,7 +9,8 @@ exports.getNotifications = async (req, res) => {
       data: notifications.map(n => {
         const obj = n.toObject();
         if (obj.data?.requestId) {
-          obj.data.requestId = obj.data.requestId.toString(
+          obj.data.requestId = obj.data.requestId.toString();
+        }
         return { ...obj, id: n._id.toString() };
       }) 
     });
